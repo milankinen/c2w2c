ae7db25b4bf033fc7da17131e2e8129119f91d53
Matti Lankinen <m.lankinen@iki.fi>
2016-06-08 17:07:35 +0300

    Remove obsolete activation arg

--- src/models/c2w.py
+++ src/models/c2w.py
@@ -1,15 +1,14 @@
-from keras.layers import LSTM, Embedding, Input, Dense, Activation, merge
+from keras.layers import LSTM, Embedding, Input, Dense, merge
 from keras.models import Model
 
 
-def C2W(V_C, V_W, d_Wi, d_W, d_C, activation=None):
+def C2W(V_C, V_W, d_Wi, d_W, d_C):
   """
     V_C        :: character vocabulary
     V_W        :: word vocabulary
     d_Wi       :: intermediate word state sequence dimension
     d_W        :: word embedding dimension ("word properties")
     d_C        :: character embedding dimension ("character properties")
-    activation :: activation to be used if model is trained separately
   """
 
   # V_W.dim[1] = max word length
@@ -23,8 +22,5 @@ def C2W(V_C, V_W, d_Wi, d_W, d_C, activation=None):
   s_Ef      = Dense(d_W)(forward)
   s_Eb      = Dense(d_W)(backwards)
   s_E       = merge(inputs=[s_Ef, s_Eb], mode='sum')
-
-  if activation is not None:
-    s_E = Activation(activation)(s_E)
 
   return Model(input=indices, output=s_E)
