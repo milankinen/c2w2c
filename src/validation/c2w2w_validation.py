ad428021fb4a26ae520248c2dda72f1a5a0ce2cf
Matti Lankinen <m.lankinen@iki.fi>
2016-06-23 23:06:03 +0300

    Refactor c2w2w validation

--- src/validation/c2w2w_validation.py
+++ src/validation/c2w2w_validation.py
@@ -2,23 +2,7 @@ import numpy as np
 import sys
 
 from ..constants import UNK
-from ..training import _prepare_data, _to_c2w2w_samples
-
-
-def _calc_perplexity(V_W, expectations, predictions):
-  tot_loss  = 0.
-  n_oov     = 0
-  n_tested  = 0
-  for idx, expected in enumerate(expectations):
-    if not V_W.has(expected):
-      n_oov += 1
-      continue
-    P_all     = predictions[idx]
-    word_loss = -np.log(P_all[V_W.get_index(expected)] / np.sum(P_all))
-    tot_loss += word_loss
-    n_tested += 1
-
-  return (0.0 if n_tested == 0 else np.exp(tot_loss / n_tested)), n_oov, n_tested
+from ..datagen import _prepare_data, _to_c2w2w_samples
 
 
 def _calc_loss(V_W, predictions, expectations):
@@ -40,7 +24,7 @@ def _calc_loss(V_W, predictions, expectations):
   return l, o, t
 
 
-def _test_model(c2w2w, n_samples, n_batch, generator, V_W):
+def _calc_pp(c2w2w, n_samples, n_batch, generator, V_W):
   l, o, t, n = 0., 0, 0, 0
   c2w2w.reset_states()
   while n < n_samples:
@@ -53,7 +37,6 @@ def _test_model(c2w2w, n_samples, n_batch, generator, V_W):
     n += len(expectations)
 
   assert n == n_samples
-
   pp    = sys.float_info.max if t == 0 else np.exp(l / t)
   oovr  = 0 if t + o == 0 else o / float(t + o)
   return pp, oovr
@@ -65,13 +48,7 @@ def make_c2w2w_test_function(c2w2w, params, dataset, V_C, V_W):
     W_np1   = list([s[1] for s in samples])
     return X, W_np1
 
-  n_batch = params.n_batch
+  n_batch              = params.n_batch
   n_samples, generator = _prepare_data(n_batch, dataset, to_test_samples, shuffle=False)
-
-  def test_model(limit=None):
-    if limit is not None:
-      print 'WARN: limit is not used anymore'
-    return _test_model(c2w2w, n_samples, n_batch, generator, V_W)
-
-  return test_model
+  return (lamda : _calc_pp(c2w2w, n_samples, n_batch, generator, V_W))
 
