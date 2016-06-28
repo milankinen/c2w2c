f5fb36875b09926effdae46a92497d01fa04e777
Matti Lankinen <m.lankinen@iki.fi>
2016-06-28 16:27:22 +0300

    Use two LSTM LM’s instead of single huge one

--- src/models/lm.py
+++ src/models/lm.py
@@ -16,16 +16,21 @@ class LanguageModel(Model):
     w_nmask       = Input(batch_shape=(n_batch, 1), name='w_nmask', dtype='int8')
 
     # Prevent padded samples to affect internal state (and cause NaN loss in worst
-    # case) by masking them by using another input value
+    # case) by masking them by using w_nmask masking values
     w_nmasked     = LMMask(0.)([Reshape((1, d_W))(w_n), w_nmask])
 
     # Using stateful LSTM for language model - model fitting code resets the
     # state after each sentence
     w_np1Ei       = LSTM(d_L,
                          trainable=trainable,
-                         return_sequences=False,
+                         return_sequences=True,
                          stateful=True,
                          consume_less='gpu')(w_nmasked)
+    w_np1Ei       = LSTM(d_L,
+                         trainable=trainable,
+                         return_sequences=False,
+                         stateful=True,
+                         consume_less='gpu')(w_np1Ei)
 
     w_np1E        = Projection(d_W)(w_np1Ei)
 
