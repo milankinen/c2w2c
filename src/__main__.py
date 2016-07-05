f286c31da36f590e49b7b0d1034dc64e50f6a78a
Matti Lankinen <m.lankinen@iki.fi>
2016-07-05 22:53:19 +0300

    Use new C2W2C model and use Keras evaluate_generator for validation and PP

--- src/__main__.py
+++ src/__main__.py
@@ -6,45 +6,39 @@ import numpy as np
 from keras.optimizers import Adam
 
 import model_params
-from c2w2c import C2W2C, build_c2w2c_validation_models
-from c2w2w import C2W2W
-from models import W2C
-from datagen import prepare_c2w2c_training_data, prepare_c2w2w_training_data, prepare_w2c_training_data
-from datagen import sample_c2w2c_text, sample_c2w2w_text
+import keras.engine.training as ket
+from keras import backend as K
+
+from models import C2W2C, WordLSTM
 from dataset import load_dataset, make_char_vocabulary
-from util import info, Timer, ContextReset
-from validation import make_c2w2c_test_function, make_c2w2w_test_function
+from dataset import initialize_c2w2c_data
+from textgen import generate_c2w2c_text
+from util import info, Timer
 
 sys.setrecursionlimit(40000)
 
 
 params = model_params.from_cli_args()
 params.print_params()
+print ''
 
 print 'Loading training data...'
 training_dataset = load_dataset(params.training_dataset, params.train_data_limit)
 training_dataset.print_stats()
+print ''
 
 print 'Loading test data...'
 test_dataset = load_dataset(params.test_dataset, params.test_data_limit)
 test_dataset.print_stats()
+print ''
 
 # Vocabularies
-V_C = make_char_vocabulary([test_dataset, training_dataset])
+V_C = make_char_vocabulary([training_dataset])
 V_W = training_dataset.vocabulary
 
 print 'V_C statistics:'
 print '  - Distinct characters: %d' % V_C.size
-
-
-def compile_model(model, returns_chars=True):
-  lr   = params.learning_rate
-  clip = 5.
-  adam = Adam(lr=lr, clipnorm=clip)
-  model.compile(optimizer=adam,
-                loss='categorical_crossentropy',
-                sample_weight_mode=('temporal' if returns_chars else None),
-                metrics=['accuracy'])
+print ''
 
 
 def try_load_weights(model, filename):
@@ -70,207 +64,186 @@ def delta_str(cur, prev, fmt='(%s%f)'):
   return fmt % ('-' if cur < prev else '+', abs(prev - cur)) if prev is not None else ''
 
 
-def c2w2c_from_c2w2w_weights(c2w2w_weights_file):
-  def weight_list(m):
-    return list([w.tolist() for w in m.get_weights()])
+def prepare_env(params):
+  mode       = params.mode
+  batch_size = params.batch_size
+  maxlen     = params.maxlen
+  d_C        = params.d_C
+  d_W        = params.d_W
+  d_Wi       = params.d_Wi
+  d_L        = params.d_L
+  d_D        = params.d_D
+  weights_0  = params.init_weight_file
 
-  c2w2c, (c2w, lm, w2c), _ = C2W2C(params.n_batch, params, V_C)
-  c2w2w                    = C2W2W(params.n_batch, params, V_C, V_W)
-  compile_model(c2w2c, returns_chars=True)
-  compile_model(c2w2w, returns_chars=False)
-  try_load_weights(c2w2w, c2w2w_weights_file)
-  try_load_weights(c2w2c, params.init_weight_file)
+  learning_rate = params.learning_rate
+  clipnorm      = 2.
+  optimizer     = Adam(lr=learning_rate, clipnorm=clipnorm)
 
-  #prev_w = weight_list(c2w2c)
-  #assert prev_w == weight_list(c2w2c)
-  for i in range(0, len(c2w2w.layers) - 2):
-    c2w2c.layers[i].set_weights(c2w2w.layers[i].get_weights())
-  #assert prev_w != weight_list(c2w2c)
-  return c2w2c, (c2w, lm, w2c)
+  if mode == 'C2W2C':
+    test_maxlen = max(len(w) + 1 for w in test_dataset.get_words())
 
+    trainable_model = C2W2C(batch_size=batch_size,
+                            maxlen=maxlen,
+                            d_C=d_C,
+                            d_W=d_W,
+                            d_Wi=d_Wi,
+                            d_L=d_L,
+                            d_D=d_D,
+                            V_C=V_C)
+    validation_model = C2W2C(batch_size=batch_size,
+                             maxlen=test_maxlen,
+                             d_C=d_C,
+                             d_W=d_W,
+                             d_Wi=d_Wi,
+                             d_L=d_L,
+                             d_D=d_D,
+                             V_C=V_C)
 
-def prepare_env(mode):
-  if mode == 'c2w2c':
-    # Train c2w2c model as it is
-    trainable_model, (c2w, lm, w2c), _  = C2W2C(params.n_batch, params, V_C)
-    v_c2wp1, v_w2c                      = build_c2w2c_validation_models(params, V_C)
-    compile_model(trainable_model, returns_chars=True)
-    compile_model(v_c2wp1, returns_chars=False)
-    compile_model(v_w2c, returns_chars=True)
-    try_load_weights(trainable_model, params.init_weight_file)
+    for m in [trainable_model, validation_model]:
+      m.compile(optimizer=optimizer,
+                loss='sparse_categorical_crossentropy',
+                sample_weight_mode='temporal',
+                metrics=['accuracy'])
 
-    def update_weights():
-      v_c2wp1.set_weights(c2w.get_weights() + lm.get_weights())
-      v_w2c.set_weights(w2c.get_weights())
+    try_load_weights(trainable_model, weights_0)
 
-    def save_weights(filename):
-      try_save_weights(trainable_model, filename)
+    training_data   = initialize_c2w2c_data(training_dataset, batch_size, maxlen, V_C, shuffle=True)
+    validation_data = initialize_c2w2c_data(test_dataset, batch_size, test_maxlen, V_C, shuffle=False)
 
-    def sample_text(seed, how_many):
-      update_weights()
-      sample_c2w2c_text(v_c2wp1, v_w2c, seed, how_many, V_W, V_C, params)
-
-    def prepare_training_data():
-      return prepare_c2w2c_training_data(params, training_dataset, V_C)
-
-    test_model  = make_c2w2c_test_function(trainable_model, v_c2wp1, v_w2c, params, test_dataset, V_C, V_W)
+    def gen_text(seed, how_many):
+      validation_model.set_weights(trainable_model.get_weights())
+      generate_c2w2c_text(validation_model, test_maxlen, seed, how_many)
 
     print 'Model parameters:'
-    print ' - C2W:%10s' % str(param_count(c2w))
-    print ' - LM: %10s' % str(param_count(lm))
-    print ' - W2C:%10s' % str(param_count(w2c))
+    print ' - C2W:%10s' % str(param_count(trainable_model.get_c2w()))
+    print ' - LM: %10s' % str(param_count(trainable_model.get_lm()))
+    print ' - W2C:%10s' % str(param_count(trainable_model.get_w2c()))
     print '       %s' % ('-' * 10)
-    print '       %10s' % str(sum([param_count(m) for m in [c2w, lm, w2c]]))
+    print '       %10s' % str(param_count(trainable_model))
 
-  elif mode == 'c2w2w':
-    trainable_model   = C2W2W(params.n_batch, params, V_C, V_W)
-    compile_model(trainable_model, returns_chars=False)
-    try_load_weights(trainable_model, params.init_weight_file)
-
-    def update_weights():
-      pass  # using same model for test as used in training
-
-    def save_weights(filename):
-      try_save_weights(trainable_model, filename)
-
-    def sample_text(seed, how_many):
-      sample_c2w2w_text(trainable_model, seed, how_many, V_W, V_C, params)
-
-    def prepare_training_data():
-      return prepare_c2w2w_training_data(params, training_dataset, V_C, V_W)
-
-    test_model = make_c2w2w_test_function(trainable_model, params, test_dataset, V_C, V_W)
+  elif mode == 'WORD':
+    raise NotImplementedError
 
     print 'Model parameters:'
     print ' - Total:%10s' % str(param_count(trainable_model))
-
-  elif mode == 'w2c':
-    if not params.c2w2w_weights:
-      print 'C2W2W weights are mandatory when training W2C model'
-      sys.exit(1)
-
-    # build models: w2c for training, validation models for data generation and quick PP checks
-    trainable_model       = W2C(params.maxlen, params.d_W, params.d_C, params.d_D, V_C, apply_softmax=True)
-    c2w2c, (c2w, lm, w2c) = c2w2c_from_c2w2w_weights(params.c2w2w_weights)
-    c2wp1, _              = build_c2w2c_validation_models(params, V_C)
-    compile_model(trainable_model, returns_chars=True)
-    compile_model(c2wp1, returns_chars=False)
-    c2wp1.set_weights(c2w.get_weights() + lm.get_weights())
-    trainable_model.set_weights(w2c.get_weights())
-
-    def update_weights():
-      # c2w2c needs updated weights if validating with "full" mode
-      w2c.set_weights(trainable_model.get_weights())
-
-    def save_weights(filename):
-      update_weights()
-      try_save_weights(c2w2c, filename)
-
-    def sample_text(seed, how_many):
-      update_weights()
-      sample_c2w2c_text(c2wp1, w2c, seed, how_many, V_W, V_C, params)
-
-    def prepare_training_data():
-      return prepare_w2c_training_data(c2wp1, params, training_dataset, V_C)
-
-    test_model = make_c2w2c_test_function(c2w2c, c2wp1, trainable_model, params, test_dataset, V_C, V_W)
 
   else:
     print 'Invalid mode: %s' % mode
     sys.exit(1)
 
-  def test_fn():
-    update_weights()
-    return test_model()
-
-  return trainable_model, test_fn, sample_text, save_weights, prepare_training_data
+  return trainable_model, validation_model, training_data, validation_data, gen_text
 
 
-fit_t     = Timer()
-test_t    = Timer()
+def main():
 
-prev_pp   = None
-prev_loss = None
-prev_acc  = None
+  n_epoch              = params.n_epoch
+  save_weight_filename = params.save_weight_file
+  do_validation_only   = params.test_only
+  gen_n_text_samples   = params.gen_n_samples
 
-model, run_tests, generate_text, persist_weights, prep_data = prepare_env(params.mode)
+  training_t   = Timer()
+  validation_t = Timer()
 
+  prev_pp   = None
+  prev_loss = None
+  prev_acc  = None
 
-def run_model_tests(prev_pp):
-  if params.gen_n_samples is not None:
-    print 'Generating text...'
-    n_seed = 30
-    seed   = test_dataset.sentences
-    if len(seed) > n_seed:
-      idx = np.random.randint(0, len(seed) - n_seed)
-      seed = seed[idx: idx + n_seed]
-    generate_text([w for s in seed for w in s], params.gen_n_samples)
+  if params.mode == 'C2W2C':
+    def c2w2c_weighted_objective(fn):
+      def weighted(y_true, y_pred, weights, mask=None):
+        assert mask is None
+        assert weights is not None
+        score_array = fn(y_true, y_pred)
 
-  print 'Validating model...'
-  test_t.start()
-  pp, oov = run_tests()
-  test_elapsed, test_tot = test_t.lap()
-  validation_info = '''Validation results:
-  - Perplexity:         %3g %s
-  - OOV rate:           %f
-  - Validation took:    %s
-  - Total validation:   %s''' % (pp, delta_str(pp, prev_pp, '(%s%3g)'), oov, test_elapsed, test_tot)
+        # reduce score_array to same ndim as weight array
+        ndim = K.ndim(score_array)
+        weight_ndim = K.ndim(weights)
+        score_array = K.mean(score_array, axis=list(range(weight_ndim, ndim)))
+
+        # apply sample weighting
+        score_array *= weights
+        word_scores = K.sum(score_array, axis=-1)
+        return K.mean(word_scores)
+      return weighted
+
+    # by default Keras calculates only mean which is not correct because
+    # word loss = sum(char losses), thus we need to monkey batch the
+    # weighted_objective function to return correct loss for C2W2C model
+    # ATTENTION: this might not work in later Keras versions, only tested with 1.0.5
+    ket.weighted_objective = c2w2c_weighted_objective
+
+  # ======== PREPARE MODELS AND DATA  ========
+
   print ''
-  info(validation_info)
-  return pp, oov
+  t_model, v_model, training_data, validation_data, gen_text = prepare_env(params)
 
+  def validate_model(ppprev):
+    if gen_n_text_samples:
+      print 'Generating %d text samples...' % gen_n_text_samples
+      n_seed = 30
+      start = max(0, np.random.randint(0, training_dataset.n_words - n_seed))
+      seed = training_dataset.get_words()[start: start + n_seed]
+      gen_text(seed=seed, how_many=gen_n_text_samples)
 
-best_weights          = None
-target_weight_filename  = params.save_weight_file
+    print 'Validating model...'
+    validation_t.start()
+    v_model.set_weights(t_model.get_weights())
+    v_model.reset_states()
+    n_v_samples, gen_v = validation_data[0]()
+    loss, _ = v_model.evaluate_generator(gen_v, n_v_samples)
+    pp = np.exp(loss)
+    val_elapsed, val_tot = validation_t.lap()
+    validation_info = '''Validation result:
+  - Model loss:        %f
+  - Perplexity:        %f %s
+  - OOV rate:          %f
+  - Validation took:   %s
+  - Total validation:  %s
+    ''' % (loss, pp, delta_str(pp, ppprev), validation_data[1], val_elapsed, val_tot)
+    info(validation_info)
+    return pp
 
-try:
-  if params.test_only:
-    run_model_tests(None)
+  if do_validation_only:
+    validate_model(None)
     sys.exit(0)
 
   print 'Training model...'
-  for e in range(0, params.n_epoch):
-    fit_t.start()
-    epoch = e + 1
+  for epoch in range(1, n_epoch + 1):
     print '=== Epoch %d ===' % epoch
+    training_t.start()
 
-    n_samples, meta, data_generator = prep_data()
+    n_t_samples, gen_t = training_data[0]()
 
-    model.reset_states()
-    h = model.fit_generator(generator=data_generator,
-                            samples_per_epoch=n_samples,
-                            callbacks=[ContextReset(model, meta)],
-                            nb_epoch=1,
-                            verbose=1)
-    fit_elapsed, fit_tot = fit_t.lap()
+    t_model.reset_states()
+    h = t_model.fit_generator(generator=gen_t,
+                              samples_per_epoch=n_t_samples,
+                              nb_epoch=1,
+                              verbose=1)
+    fit_elapsed, fit_tot = training_t.lap()
 
     loss       = h.history['loss'][0]
     acc        = h.history['acc'][0]
     epoch_info = '''Epoch %d summary at %s:
   - Model loss:         %f %s
   - Model accuracy:     %f %s
+  - Perplexity:         %f
   - Training took:      %s
   - Total training:     %s''' % (epoch, strftime("%Y-%m-%d %H:%M:%S", localtime()), loss, delta_str(loss, prev_loss),
-                                 acc, delta_str(acc, prev_acc), fit_elapsed, fit_tot)
+                                 acc, delta_str(acc, prev_acc), np.exp(loss), fit_elapsed, fit_tot)
     print ''
     info(epoch_info)
 
-    # persist weights anyway (if weight file is defined)
-    persist_weights(target_weight_filename)
+    pp = validate_model(prev_pp)
 
-    # needed for validation models LM and W2C
-    pp, _ = run_model_tests(prev_pp)
 
-    if prev_pp is None or pp <= prev_pp:
-      # persist '.best' weight file only if PP has improved since last "best"
-      if prev_pp is not None and target_weight_filename is not None:
-        persist_weights(target_weight_filename + '.best')
-      best_weights = np.array(model.get_weights())
-      prev_acc  = acc
-      prev_loss = loss
-      prev_pp   = pp
+    prev_acc  = acc
+    prev_loss = loss
+    prev_pp   = pp
 
   print 'Training complete'
-except KeyboardInterrupt:
-  print 'Training interrupted. Bye'
 
+
+try:
+  main()
+except KeyboardInterrupt:
+  print 'Interrupted, kthxbye.'
