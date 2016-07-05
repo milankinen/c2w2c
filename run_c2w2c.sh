2b7c90ae560a1caa9b7bbb64efacebf4f275e3f6
Matti Lankinen <m.lankinen@iki.fi>
2016-07-06 00:18:07 +0300

    Add shorthand scripts for model running

---
+++ run_c2w2c.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export DEVICE=cpu
+
+./run --training data/training.txt \
+      --test data/validation.txt \
+      --save-weights c2w2c.h5 \
+      --load-weights c2w2c.h5 \
+      --mode c2w2c \
+      -w 21
---
+++ run_word_lstm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export DEVICE=cpu
+
+./run --training data/training.txt \
+      --test data/validation.txt \
+      --save-weights word.h5 \
+      --load-weights word.h5 \
+      --mode word
